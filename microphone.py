0cd4a2b950bf2bb2d6af974f6035ef62ac1396b1
dgole <dgole100@gmail.com>
2017-11-11 18:56:40 -0700

    Update microphone.py

--- microphone.py
+++ microphone.py
@@ -5,26 +5,25 @@ import pyaudio
 import config
 
 
-def start_stream(callback):
-    p = pyaudio.PyAudio()
-    frames_per_buffer = int(config.MIC_RATE / config.FPS)
-    stream = p.open(format=pyaudio.paInt16,
-                    channels=1,
-                    rate=config.MIC_RATE,
-                    input=True,
-                    frames_per_buffer=frames_per_buffer)
-    overflows = 0
-    prev_ovf_time = time.time()
-    while True:
+def Stream():
+    def __init__(self):
+        self.p = pyaudio.PyAudio()
+        self.frames_per_buffer = int(config.MIC_RATE / config.FPS)
+        self.stream = self.p.open(format=pyaudio.paInt16,
+                        channels=1,
+                        rate=config.MIC_RATE,
+                        input=True,
+                        frames_per_buffer=self.frames_per_buffer)
+        self.overflows = 0
+        self.prev_ovf_time = time.time()
+    def getData(self):
         try:
             y = np.fromstring(stream.read(frames_per_buffer), dtype=np.int16)
             y = y.astype(np.float32)
-            callback(y)
+            return(y)
         except IOError:
-            overflows += 1
-            if time.time() > prev_ovf_time + 1:
-                prev_ovf_time = time.time()
-                print('Audio buffer has overflowed {} times'.format(overflows))
-    stream.stop_stream()
-    stream.close()
-    p.terminate()
+            self.overflows += 1        
+    def stopStream(self):
+        self.stream.stop_stream()
+        self.stream.close()
+        self.p.terminate()
